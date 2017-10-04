dc26b70dfe278e6f3bff2a661ee45fb8daa54756
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-04 23:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const app = "I don't do much."
 
-var kittens = ["Milo", "Otis", "Garfield"];
+function reset() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+reset()
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
+
+reset()
 
 function destructivelyPrependKitten(name) {
   kittens.pop(name);
