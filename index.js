9a518ea7cb6312f7756270443febb75b2e7a72a9
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-04 23:43:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyAppendKitten(name) {
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyPrependKitten(name) {
-  kittens.pop(name);
+  kittens.unshift(name);
   return kittens;
 }
